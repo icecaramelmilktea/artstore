f2d209c32349b2514be5cd540e67190e73758fd9
JerryLu <icecaramelmilktea@yahoo.com.tw>
2016-03-14 14:44:21 +0800

    homework-w2 step5 quantity > 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,12 +11,22 @@ class CartItemsController < ApplicationController
     redirect_to :back
   end
 
+
   def update
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
-    @item.update(item_params)
+
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+
     redirect_to carts_path
   end
+
+
   private
   def item_params
     params.require(:cart_item).permit(:quantity)
